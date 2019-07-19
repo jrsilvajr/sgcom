3c1ebcfbf09ce116652289a71a3e60ecd5587b36
Silva <jrjrsilva@gmail.com>
2019-07-18 22:22:18 -0300

    atualizando depois do pull request

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    protected $redirectTo = '/Admin';
 
     /**
      * Create a new controller instance.
